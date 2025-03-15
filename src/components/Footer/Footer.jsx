fac344603a9486472f62b83a45a8e2919ec57a4a
David smart <112566573+davidgraphix@users.noreply.github.com>
2025-03-15 11:26:24 +0100

    Created all the files and add their functionalities

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Footer/Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>
+        Copyright @ 2025, Cryptoplace App, Developed by DavidSmart - All Right
+        Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
